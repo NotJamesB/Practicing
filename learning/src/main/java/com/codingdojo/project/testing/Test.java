e5e8729214010e8cc4852e373aba1261ab367822
James Bucsko <97855508+NotJamesB@users.noreply.github.com>
2022-11-28 17:23:55 -0800

    Add files via upload

---
+++ learning/src/main/java/com/codingdojo/project/testing/Test.java
@@ -0,0 +1,36 @@
+package com.codingdojo.project.testing;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+//		Scanner se = new Scanner(System.in);
+//		System.out.println("Enter number: ");
+//		int num = se.nextInt();
+//		System.out.println("Enter number: ");
+//		int num2 = se.nextInt();
+//		int sum = num + num2;
+//		System.out.println("The number is " + sum);
+//		
+//		Scanner s = new Scanner(System.in);
+//		System.out.println("Enter your budget: ");
+//		int budget = s.nextInt();
+//		System.out.println("Enter the price of the item you're buying: ");
+//		int price = s.nextInt();
+//		int remaining = budget - price;
+//		if(remaining <= 0) {
+//			System.out.println("You cannot afford this");
+//		}
+//		else {
+//			System.out.println("You have " + remaining + " balance remaining.");
+//		}
+		
+		String gameArray[];
+		gameArray = new String[5];
+		gameArray
+		
+		
+	}
+
+}
